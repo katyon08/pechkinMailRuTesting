91aba38b07f17bb2c59f20b9990b4cfbf88d68c1
katyon08 <katyon08@yandex.ru>
2015-11-12 22:48:04 +0200

    v_1.0 First 3 smoke tests. Passed test: 2/3; Head title test is not passed.

---
+++ src/main/java/pages/RegistrationPage.java
@@ -0,0 +1,9 @@
+package pages;
+
+/**
+ * Created by katyon08 on 12.11.2015.
+ */
+public class RegistrationPage extends AbstractPage {
+
+
+}
--- src/test/java/seleniumTests/AbstractTest.java
+++ src/test/java/seleniumTests/AbstractTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.log4testng.Logger;
 import pages.AbstractPage;
 
 import java.util.concurrent.TimeUnit;
@@ -12,6 +13,8 @@ import java.util.concurrent.TimeUnit;
 public class AbstractTest {
 
     private static WebDriver ffdriver;
+
+    private static Logger logger;
 
     public static WebDriver getWebDriver() {
         return ffdriver;
@@ -32,6 +35,7 @@ public class AbstractTest {
     @BeforeTest
     public void initializeAbstractPageDriver() {
         AbstractPage.setFFDriver(ffdriver);
+        logger = Logger.getLogger(AbstractTest.class);
     }
 
     @AfterTest
